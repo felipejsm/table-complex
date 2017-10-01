b2bfd023e077fcc70b60e8f8e7fece32763e5ac0
felipejsm <felipejsm41@gmail.com>
2017-10-01 19:29:52 -0300

    Estrutura complexa criada no js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
-  
+  var vm = $scope;
+  vm.estrutura = [tipo1={estado:'São Paulo',capital:'São Paulo',sigla:'SP'},
+                  tipo2={bairro:'Capão Redondo',zona:'Sul'}];
 }]);
