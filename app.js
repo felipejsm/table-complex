7fea3a84a8526f2f45d4ba36352724eaf7948f0e
felipejsm <felipejsm41@gmail.com>
2017-10-01 19:57:35 -0300

    Melhorando a tabela ng-repeat

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
   var vm = $scope;
-  vm.estrutura = [tipo1={estado:'São Paulo',capital:'São Paulo',sigla:'SP'},
-                  tipo2={bairro:'Capão Redondo',zona:'Sul'}];
+  vm.estrutura = [{tipo1:[{estado:'São Paulo',capital:'São Paulo',sigla:'SP'}],
+                  tipo2:[{bairro:'Capão Redondo',zona:'Sul', show:false}]}];
 }]);
