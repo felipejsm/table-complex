6a260492eed6beaa9f071a1027d16620dad2cdf1
felipejsm <felipejsm41@gmail.com>
2017-10-03 07:33:31 -0300

    Show JSON content

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
   var vm = $scope;
-  vm.estrutura = [{tipo1:[{estado:'São Paulo',capital:'São Paulo',sigla:'SP'}],
-                  tipo2:[{bairro:'Capão Redondo',zona:'Sul', show:false}]}];
+  vm.estrutura = [{
+              "tipo1":[
+                  {'estado':'São Paulo','capital':'São Paulo','sigla':'SP'}],
+              "tipo2":[
+                  {"bairro":'Capão Redondo',"zona":'Sul', "show":false}]
+                  }];
 }]);
