a432b0039c747dc0a69dd6a06a0ccdfb19015a71
felipejsm <felipejsm41@gmail.com>
2017-10-10 07:35:33 -0300

    Some progress

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('myCtrl', ['$scope', function($scope) {
+myApp.controller('myCtrl', ['$scope','$filter', function($scope,$filter) {
   var vm = $scope;
   vm.estrutura = [{
               "tipo1":[
-                  {'estado':'São Paulo','capital':'São Paulo','sigla':'SP'}],
+                  {'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true}],
               "tipo2":[
                   {"bairro":'Capão Redondo',"zona":'Sul', "show":false}]
                   }];
+  vm.showLine = showLine;
+
+  function showLine (isShow) {
+    vm.estrutura[0].tipo2[0].show = isShow;
+  }
 }]);
