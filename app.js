2f6e1735cf81bf23f7c39211d361f98823f5a6e7
felipejsm <felipejsm41@gmail.com>
2017-10-11 07:47:16 -0300

    List item count in for

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('myCtrl', ['$scope','$filter', function($scope,$filter) {
+myApp.controller('myCtrl', ['$scope', function($scope) {
   var vm = $scope;
   vm.showMe = false;
-  vm.estrutura = [{
-              "tipo1":[
-                  {'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true}],
-              "tipo2":[
-                  {"bairro":'Capão Redondo',"zona":'Sul', "show":false}]
-                  }];
+  vm.listarComRegras = listarComRegras;
+  vm.lista = [];
+  vm.lista.push({num:0,impar:'impar'});
+  vm.lista.push({num:0,par:'par'});
+  vm.lista.push({num:0,impar:'impar'});
+  vm.lista.push({num:0,par:'par'});
+  vm.lista.push({num:0,impar:'impar'});
+  vm.lista.push({num:0,par:'par'});
+  vm.lista.push({num:0,impar:'impar'});
+  vm.lista.push({num:0,par:'par'});
+
+  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP'}];
   /*              
   vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true},
                     "tipo2":subestrutura                      
@@ -20,5 +26,14 @@ myApp.controller('myCtrl', ['$scope','$filter', function($scope,$filter) {
   function showLine (isShow) {
     //vm.estrutura[0].tipo2[0].show = isShow;
     vm.showMe = isShow;
+  }
+  vm.listarComRegras();
+  function listarComRegras() {
+    for(var index = 0; index < vm.lista.length;index++) {
+      if(vm.lista[index].par !== undefined) {
+        vm.lista[index].num++;
+      }
+      console.log(vm.lista[index]);
+    }
   }
 }]);
