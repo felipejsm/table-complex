ba9cede795ae8053946a28c6f2b8dc87691bd5e3
felipejsm <felipejsm41@gmail.com>
2017-10-10 21:32:56 -0300

    Plain Old Html table with some ang1

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope','$filter', function($scope,$filter) {
   var vm = $scope;
+  vm.showMe = false;
   vm.estrutura = [{
               "tipo1":[
                   {'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true}],
               "tipo2":[
                   {"bairro":'Capão Redondo',"zona":'Sul', "show":false}]
                   }];
+  /*              
+  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true},
+                    "tipo2":subestrutura                      
+                  ];  
+                  */              
+  vm.subestrutura = {"bairro":'Capão Redondo',"zona":'Sul', "show":false};
   vm.showLine = showLine;
 
   function showLine (isShow) {
-    vm.estrutura[0].tipo2[0].show = isShow;
+    //vm.estrutura[0].tipo2[0].show = isShow;
+    vm.showMe = isShow;
   }
 }]);
