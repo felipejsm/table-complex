f14cb13573d7854d99c264c25f8074ce66e15368
felipejsm <felipejsm41@gmail.com>
2017-10-12 09:01:44 -0300

    Usin ng-repeat

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
   vm.lista.push({num:0,impar:'impar'});
   vm.lista.push({num:0,par:'par'});
 
-  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP'}];
+  const subestrutura = [{selecione:'Selecione',Ma:'Ma',Mp:'Mp',FMa:'FMa',FMp:'FMp',m_a:'m(a)',m_p:'m(p)',FC:'FC',
+  FD:'FD',CF:'CF',C:'C',Cn:'Cn',FC_linha: 'FC\'',C_linha_F:'C\'F',C_linha:'C\'',FT:'FT',
+  TF:'TF',T:'T',FV:'FV',VF:'VF',V:'V',FY:'FY',YF:'YF',Y:'Y',Fr:'Fr',rF:'rF',F:'F'}];
+  
+  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP'},
+                 subestrutura];
   /*              
   vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true},
                     "tipo2":subestrutura                      
                   ];  
                   */              
-  vm.subestrutura = {"bairro":'Capão Redondo',"zona":'Sul', "show":false};
+ // vm.subestrutura = {"bairro":'Capão Redondo',"zona":'Sul', "show":false};
   vm.showLine = showLine;
 
   function showLine (isShow) {
@@ -28,6 +33,8 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
     vm.showMe = isShow;
   }
   vm.listarComRegras();
+
+
   function listarComRegras() {
     for(var index = 0; index < vm.lista.length;index++) {
       if(vm.lista[index].par !== undefined) {
