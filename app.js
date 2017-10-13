3238b6b082c0c6cfd8ff9abb945458c650b816d9
felipejsm <felipejsm41@gmail.com>
2017-10-13 17:18:14 -0300

    First working version

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
   var vm = $scope;
-  vm.showMe = false;
   vm.listarComRegras = listarComRegras;
   vm.lista = [];
   vm.lista.push({num:0,impar:'impar'});
@@ -18,23 +17,8 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
   FD:'FD',CF:'CF',C:'C',Cn:'Cn',FC_linha: 'FC\'',C_linha_F:'C\'F',C_linha:'C\'',FT:'FT',
   TF:'TF',T:'T',FV:'FV',VF:'VF',V:'V',FY:'FY',YF:'YF',Y:'Y',Fr:'Fr',rF:'rF',F:'F'}];
   
-  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP'},
-                 subestrutura];
-  /*              
-  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP',"show":true},
-                    "tipo2":subestrutura                      
-                  ];  
-                  */              
- // vm.subestrutura = {"bairro":'Capão Redondo',"zona":'Sul', "show":false};
-  vm.showLine = showLine;
-
-  function showLine (isShow) {
-    //vm.estrutura[0].tipo2[0].show = isShow;
-    vm.showMe = isShow;
-  }
-  vm.listarComRegras();
-
-
+  vm.estrutura = [{'estado':'São Paulo','capital':'São Paulo','sigla':'SP','isCollapsed':false},subestrutura];
+  
   function listarComRegras() {
     for(var index = 0; index < vm.lista.length;index++) {
       if(vm.lista[index].par !== undefined) {
@@ -43,4 +27,5 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
       console.log(vm.lista[index]);
     }
   }
+  vm.listarComRegras();
 }]);
